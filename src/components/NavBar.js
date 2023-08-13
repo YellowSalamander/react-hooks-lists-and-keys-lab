78aa6b55f29d4472b56cb0d9759f691c72c53bf2
Samuel Rivero <samuelfras@gmail.com>
2023-08-13 12:41:30 -0600

    Lab complete

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,13 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return <nav>
+    {links.map ((links) =>
+    ( <a key= {links} href= {`#${links}`} name={`#${links}`}> 
+    {links}
+    </a>
+    ))
+      /* display an <a> tag for each link here */}</nav>;
 }
 
 export default NavBar;
